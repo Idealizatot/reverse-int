dc7fa04744827deee58ce7f12a50a2785a0ebdab
Idealizatot <ideal103.5@gmail.com>
2021-02-28 21:29:41 +0300

    feat: new function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+	if (n < 0) {
+		n *= -1;
+	}
+	let strArr = ('' + n).split('');
+	for (let i = 0; i < strArr.length / 2; i++) {
+		let tmp = strArr[i];
+		strArr[i] = strArr[strArr.length - i - 1];
+		strArr[strArr.length - i - 1] = tmp;
+	}
+
+	return + strArr.join('');
 }
